006dbc788015b1de39c0063172f59fb29b5b76f6
SioazinoNapoporitan <nahoko58osawa@gmail.com>
2026-03-06 17:54:44 +0900

    Scenescriptのバグを修正しました。 ・コルーチンを使用するためのUsing文が抜けてました。 ・存在しない関数を参照していました。

--- SceneScript.cs
+++ SceneScript.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 using UnityEngine.SceneManagement;
 using UnityEngine.EventSystems;
 
@@ -6,7 +7,7 @@ public class SceneScript : MonoBehaviour
 {
     private string _SceneName;
     private int _WaitTime;
-    
+
     public void SettingSeneName(string ScenenName)
     {
         _SceneName = ScenenName;
@@ -18,32 +19,32 @@ public class SceneScript : MonoBehaviour
     //次のシーンへ　※上記二つを設定する必要があります※
     public void NextScene()
     {
-        StartCoroutine(TitleScene(_SceneName,_WaitTime));
+        StartCoroutine(_NextScene(_SceneName, _WaitTime));
     }
     //リスタート
     public void ReloadScene(float waitTime)
     {
-        StartCoroutine(_ReloadScene(WaitTime));
+        StartCoroutine(_ReloadScene(waitTime));
     }
     //ゲーム終了
     public void Quit(BaseEventData data)
     {
         Application.Quit();
     }
-    
 
-     IEnumerator _NextScene(string ScenenName, int waitTime)
+
+    IEnumerator _NextScene(string ScenenName, int waitTime)
     {
-        yield return new WaitForSeconds(WaitTime);
+        yield return new WaitForSeconds(waitTime);
         SceneManager.LoadScene(ScenenName);
     }
 
-        IEnumerator _ReloadScene(float WaitTime)
+    IEnumerator _ReloadScene(float WaitTime)
     {
         Debug.Log("Gameをロード");
         yield return new WaitForSeconds(WaitTime);
-        SceneManager.LoadScene(SceneManager.GetActiveSene().name);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
-    
+
 }
